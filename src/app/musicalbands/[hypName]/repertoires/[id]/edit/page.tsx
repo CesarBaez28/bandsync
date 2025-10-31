e65205af2c6ce8b532e0b00d65a74df78bf372b9
CesarBaez28 <baezcesar329@gmail.com>
2025-10-30 22:50:03 -0400

    refactor: replace auth session handling with direct API call to get musical band data

--- src/app/musicalbands/[hypName]/repertoires/[id]/edit/page.tsx
+++ src/app/musicalbands/[hypName]/repertoires/[id]/edit/page.tsx
@@ -1,20 +1,20 @@
 import { getAllSongsByMusicalBandId } from "@/app/lib/api/songs";
 import { ApiResponse, Repertoire, Song } from "@/app/lib/definitions";
 import { handleAsync } from "@/app/lib/utils";
-import { auth } from "@/auth";
 import styles from '../../repertoires.module.css'
 import { getRepertoireById, getRepertoireSongs } from "@/app/lib/api/repertoires";
-import { UUID } from "crypto";
+import { UUID } from "node:crypto";
 import Form from "@/app/ui/musicalbands/repertoires/EditForm";
+import { getMusicalBandByHyphenatedName } from "@/app/lib/api/musicalBands";
 
 type EdtitRepertoiresPageProps = {
   params: Promise<{ hypName: string; id: UUID }>;
 }
 
 export default async function EditRepertoirePage(props: EdtitRepertoiresPageProps) {
-  const [session, { hypName, id }] = await Promise.all([auth(), props.params]);
+  const { hypName, id } = await props.params;
 
-  const musicalBand = session?.user?.musicalBands.find(mb => mb.hyphenatedName === hypName);
+  const musicalBand = (await getMusicalBandByHyphenatedName({ name: hypName })).data;
 
   const [[songs, error], [repertoire, error2], [repertoireSongs, error3]] = await Promise.all(
     [
--- src/app/musicalbands/[hypName]/repertoires/[id]/see/page.tsx
+++ src/app/musicalbands/[hypName]/repertoires/[id]/see/page.tsx
@@ -1,24 +1,24 @@
+import { getMusicalBandByHyphenatedName } from '@/app/lib/api/musicalBands';
 import styles from '../../repertoires.module.css';
 import { getRepertoireById, getRepertoireSongs } from "@/app/lib/api/repertoires";
 import { ApiResponse, Repertoire, Song } from "@/app/lib/definitions";
 import { handleAsync } from "@/app/lib/utils";
 import RepertoireSongsTable from '@/app/ui/musicalbands/repertoires/SeeRepertoireSongsTable';
-import { auth } from "@/auth";
-import { UUID } from "crypto";
+import { UUID } from "node:crypto";
 
 type Props = {
   params: Promise<{ hypName: string; id: UUID }>;
 }
 
 export default async function SeeRepertoireSongsPage(props: Props) {
-  const [session, { hypName, id }] = await Promise.all([auth(), props.params]);
+  const { hypName, id } = await props.params;
 
-  const musicalBand = session?.user?.musicalBands.find(mb => mb.hyphenatedName === hypName);
+  const musicalBand = (await getMusicalBandByHyphenatedName({ name: hypName })).data;
 
 
   const [[songs, error], [repertoire, error2]] = await Promise.all([
     handleAsync<ApiResponse<Song[]>>(getRepertoireSongs({ repertoireId: id, musicalBandId: musicalBand?.id })),
-    handleAsync<ApiResponse<Repertoire>>(getRepertoireById({repertoireId: id, musicalBandId: musicalBand?.id}))
+    handleAsync<ApiResponse<Repertoire>>(getRepertoireById({ repertoireId: id, musicalBandId: musicalBand?.id }))
   ]);
 
   return (
@@ -31,7 +31,7 @@ export default async function SeeRepertoireSongsPage(props: Props) {
             <p>Hubo un error al cargar la página. Intente refrescar la página o vuelva a visitar la página más tarde.</p>
           </div>
           : (
-            <RepertoireSongsTable songs={songs?.data}/>
+            <RepertoireSongsTable songs={songs?.data} />
           )
         }
       </main>
