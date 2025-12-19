7f8519c8372aa9dc46a59a2ac72320d0586c66cf
CesarBaez28 <baezcesar329@gmail.com>
2025-12-19 14:41:06 -0400

    feat: add layout and styling for MusicalBandPage with options menu

--- src/app/musicalbands/[hypName]/page.tsx
+++ src/app/musicalbands/[hypName]/page.tsx
@@ -1,9 +1,131 @@
-export default async function MusicalBandPage() {
+import { ReactNode } from 'react';
+import styles from './page.module.css'
+import Image from 'next/image';
+import CustomLink from '@/app/ui/link/CustomLink';
+
+type MenuItem = {
+  label: string;
+  icon?: ReactNode;
+  subItems?: { label: string, href: string }[]
+}
+
+type Props = {
+  params: Promise<{ hypName: string; }>;
+}
+
+export default async function MusicalBandPage(props: Props) {
+  const hypName = (await props.params).hypName;
+
+  const menuItems: MenuItem[] = [
+    {
+      label: 'Integrantes',
+      icon: <Image src="/group_24dp.svg" alt='Ingegrantes' width={36} height={36} />,
+      subItems: [
+        {
+          label: 'Integrantes',
+          href: `/musicalbands/${hypName}/users`
+        }
+      ]
+    },
+    {
+      label: 'Repertorios',
+      icon: <Image src='/library_music_24dp.svg' alt='Repertorios' width={36} height={36} />,
+      subItems: [
+        {
+          label: 'Ver repertorios',
+          href: `/musicalbands/${hypName}/repertoires`
+        },
+        {
+          label: 'Crear repertorio',
+          href: `/musicalbands/${hypName}/repertoires/create`
+        },
+        {
+          label: 'Exportar repertorio',
+          href: `/musicalbands/${hypName}/repertoires/export`
+        }
+      ]
+    },
+    {
+      label: 'Canciones',
+      icon: <Image src='/audio_file_24dp.svg' alt='Canciones' width={36} height={36} />,
+      subItems: [
+        {
+          label: 'Ver canciones',
+          href: `/musicalbands/${hypName}/songs`
+        },
+        {
+          label: 'Registrar canción',
+          href: `/musicalbands/${hypName}/songs/create`
+        },
+        {
+          label: 'Artistas',
+          href: `/musicalbands/${hypName}/artists`
+        },
+        {
+          label: 'Géneros',
+          href: `/musicalbands/${hypName}/genres`
+        }
+      ]
+    },
+    {
+      label: 'Roles musicales',
+      icon: <Image src='/adaptive_audio_mic_24dp.svg' alt='Roles musicales' width={36} height={36} />,
+      subItems: [
+        {
+          label: 'Roles musicales',
+          href: `/musicalbands/${hypName}/musical-roles`
+        }
+      ]
+    },
+    {
+      label: 'Calendario',
+      icon: <Image src='/calendar_month_24dp.svg' alt='Calendario' width={36} height={36} />,
+      subItems: [
+        {
+          label: 'Calendario',
+          href: `/musicalbands/${hypName}/calendar`
+        }
+      ]
+    },
+    {
+      label: 'Configuración',
+      icon: <Image src='/settings_24dp.svg' alt='Ingegrantes' width={36} height={36} />,
+      subItems: [
+        {
+          label: 'Banda',
+          href: `/musicalbands/${hypName}/settings`
+        },
+        {
+          label: 'Roles y permisos',
+          href: `/musicalbands/${hypName}/roles-and-permissions`
+        }
+      ]
+    },
+  ]
+
   return (
     <main>
-      <div>
-        <h2>Menú principal</h2>
-      </div>
+      <section className={styles.optionsContainer}>
+
+        {menuItems.map((item) => (
+          <article className={styles.optionContainer} key={item.label}>
+            <header className={styles.optionHeader}>
+              <h2>{item.label}</h2>
+              {item.icon}
+            </header>
+            <ul className={styles.itemsContainer}>
+              {item.subItems?.map((subItem) => (
+                <li className={styles.item} key={subItem.label}>
+                  <CustomLink variant='tertiary' href={subItem.href}>
+                    {subItem.label}
+                  </CustomLink>
+                </li>
+              ))}
+            </ul>
+          </article>
+        ))}
+
+      </section>
     </main>
   );
 }
