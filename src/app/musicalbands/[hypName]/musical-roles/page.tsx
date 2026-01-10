f130a580153cb89a6ee5d4fd3abf71b48c1f31f1
CesarBaez28 <baezcesar329@gmail.com>
2026-01-09 20:33:04 -0400

    feat: implement data provider and loading skeletons for musical roles page

--- src/app/musicalbands/[hypName]/musical-roles/page.tsx
+++ src/app/musicalbands/[hypName]/musical-roles/page.tsx
@@ -1,12 +1,11 @@
 import { getMusicalBandByHyphenatedName } from '@/app/lib/api/musicalBands';
 import styles from './musical-roles.module.css';
-import { getMusicalRolesByMusicalBandIdAndName } from "@/app/lib/api/musicalRoles";
-import { ApiResponse, MusicalRole, PagedData } from "@/app/lib/definitions";
-import { handleAsync } from "@/app/lib/utils";
 import InputContainer from '@/app/ui/musicalbands/musical-roles/InputContaimer';
-import MusicalRoleTable from '@/app/ui/musicalbands/musical-roles/MusicalRoleTable';
-import Pagination from "@/app/ui/pagination/Pagination";
 import { Metadata } from 'next';
+import { Suspense } from 'react';
+import MusicalRolesTableDataProvider from '@/app/ui/musicalbands/musical-roles/MusicalRolesTableDataProvider';
+import TableSkeleton from '@/app/ui/skeletons/TableSkeleton';
+import PaginationSkeleton from '@/app/ui/skeletons/PaginationSkeleton';
 
 type MusicalRolesPageProps = {
   params: Promise<{ hypName: string; }>;
@@ -22,19 +21,12 @@ export const metadata: Metadata = {
 };
 
 export default async function MusicalRolesPage(props: MusicalRolesPageProps) {
-  const searchParams = await props.searchParams;
-  const params = await props.params;
+  const [{ hypName }, { query = '', page = '1' } = {}] = await Promise.all([
+    props.params,
+    props.searchParams
+  ]);
 
-  const query = searchParams?.query || '';
-  const page = Number(searchParams?.page) || 1;
-  const hypName = params.hypName;
   const musicalBand = (await getMusicalBandByHyphenatedName({ name: hypName })).data;
-
-  const [response, error] = await handleAsync<ApiResponse<PagedData<MusicalRole>>>(getMusicalRolesByMusicalBandIdAndName({
-    musicalBandId: musicalBand?.id,
-    query,
-    page
-  }));
 
   return (
     <div>
@@ -43,18 +35,21 @@ export default async function MusicalRolesPage(props: MusicalRolesPageProps) {
       <InputContainer musicalBandId={musicalBand?.id} hypName={hypName} />
 
       <main className={styles.mainContainer}>
-        {error
-          ? <div className="message">
-            <h2>¡Lo sentimos!</h2>
-            <p>Hubo un error al traer los datos. Intente refrescar la página o vuelva a visitar la página más tarde.</p>
-          </div>
-          : (
-            <div>
-              <Pagination totalPages={response?.data?.totalPages ?? 0} />
-              <MusicalRoleTable data={response?.data} hypName={hypName} musicalBandId={musicalBand?.id} />
-            </div>
-          )
-        }
+
+        <Suspense fallback={
+          <>
+            <PaginationSkeleton showArrows={false} pages={3} />
+            <TableSkeleton columns={2} rows={6} />
+          </>
+        }>
+          <MusicalRolesTableDataProvider
+            musicalBandId={musicalBand?.id}
+            hypName={hypName}
+            query={query}
+            page={Number(page)}
+          />
+        </Suspense>
+
       </main>
 
     </div>
